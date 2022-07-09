69e79752fb7ae39b9dd15d92664d4991777a8a55
Hazem Kawas <hazem.k.4488@gmail.com>
2022-07-09 14:06:18 +0200

    responsive nav

--- src/Components/AboutMe/AboutMe.tsx
+++ src/Components/AboutMe/AboutMe.tsx
@@ -3,7 +3,6 @@ import Box, { BoxProps } from '@mui/material/Box';
 import { blueGrey } from '@mui/material/colors';
 import { Avatar, Typography } from '@mui/material';
 import faceImg from '../../img/img/face1.jpg'
-import './AboutMe.css'
 
 // icons
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -12,24 +11,9 @@ import { useEffect, useState } from 'react';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import TypewriterEffec from '../TypeWriterEffec';
 
-const AboutBox = styled(Box)<BoxProps>(({ theme }) => ({
-  color: theme.palette.getContrastText(blueGrey[900]),
-  width: '100%',
-  // height: '100vh',
-  // marginLeft: '8%',
-  '&:hover': {
-    // backgroundColor: blueGrey[700],
-  },
-}));
 const AboutMe = () => {
   const [gitOpen, setGitOpen] = useState(false)
   
-  // useEffect(() => {
-  //   const timer = window.setTimeout(() => {
-  //     setGitOpen(true);
-  //   }, 1000);
-  //   // return () =>  clearTimeout(timer);
-  // }, []);
   useEffect(() => {
     const timer = window.setTimeout(() => {
       gitOpen ? setGitOpen(false) : setGitOpen(true)
@@ -37,10 +21,7 @@ const AboutMe = () => {
     return () =>  clearTimeout(timer);
   }, [gitOpen]);
   return (
-    <AboutBox>
-      {/* <Box sx={titleStyle}>
-      <Typography >About Me</Typography >
-      </Box> */}
+    <Box sx={AboutBoxStyle}>
       <Box sx={aboutDiv}>
       <Box sx={aboutText}>
         <Typography className='about-mainText'>
@@ -54,29 +35,24 @@ const AboutMe = () => {
         About Me
       </Box>
       </Box>
-      <Box className='about-icons'>
+      <Box  sx={aboutIcons}>
         <GitHubIcon className='icons' />
         <LinkedInIcon  className='icons'/>
         <ContactMailIcon  className='icons'/>
         </Box>
         {gitOpen ? <>
-          <Box className= 'git'>
-          <Typography className='git-text'>
+          <Box sx={popUp}>
+          <Typography  sx={popUpText}>
             Hello..!
             Lets get in touch
           </Typography>
         </Box>
         </> : null}
-      </AboutBox>
+      </Box>
   )
 }
-const titleStyle: SxProps = {
-  width: { xs: '50', md: '50%', xl: '50%' },
-  display: 'flex',
-  alignItems: 'flex-start',
-  paddingLeft: { xs: '1em', md: '1em', lg: '2em' },
-  paddingTop: { xs: '1em', md: '1em', lg: '2em' },
-  height: '5vh',
+const AboutBoxStyle: SxProps = {
+  width: { xs: '50', md: '50%', lg: '98%' },
 }
 const aboutDiv: SxProps = {
   width: { xs: '50', md: '50%', lg: '100%', xl: '100%' },
@@ -84,7 +60,7 @@ const aboutDiv: SxProps = {
   justifyContent: 'center',
   alignItems: 'center',
   height: '90vh',
-
+  paddingLeft: {xs: '0', md: '0', lg: '1em'},
 }
 
 const aboutText: SxProps = {
@@ -92,7 +68,24 @@ const aboutText: SxProps = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-
+}
+const aboutIcons: SxProps = {
+  position: 'absolute',
+  left: '50%',
+  bottom:' 1em',
+  // transform: 'translate(-50%, -50%)',
+}
+const popUp: SxProps = {
+  width: '150px',
+  height: '50px',
+  backgroundColor: '#fff',
+  position: 'absolute',
+  left: '55%',
+  bottom: '3em',
+  transform: 'translate(+47%, -40%)',
+  borderRadius: '1em 1em  1em 0.1em',
+  display: 'flex',
+  alignItems: 'center',
 }
 const aboutInfo: SxProps = {
   width: { xs: '50', md: '50%', lg: '100%' },
@@ -101,4 +94,11 @@ const aboutInfo: SxProps = {
   alignItems: 'center',
 
 }
+
+const popUpText: SxProps = {
+  color: 'rgb(141, 15, 15)',
+  fontSize: '.9em',
+  padding: '1em',
+}
+
 export default AboutMe
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -34,79 +34,79 @@ const NavBar = () => {
       // backgroundColor: blueGrey[700],
     },
   }));
-  const NavBox = styled(Box)<BoxProps>(({ theme }) => ({
-    color: theme.palette.getContrastText(blueGrey[900]),
-    height: '100vh',
-    width: '6%',
-    borderRight: '1px solid #d1c4e9',
-    transition: 'all .6s',
-    position: 'absolute',
-    '&:hover': {
-      backgroundColor: '#022c33',
-      width: '12%',
-      borderRight: 'none',
-    },
-  }));
-  const AvtarBox = styled(Box)<BoxProps>(({ theme }) => ({
-    color: theme.palette.getContrastText(blueGrey[900]),
-      display: 'flex',
-      alignItems: 'center',
-      cursor: 'pointer',
-    '&:hover': {
-      // backgroundColor: blueGrey[700],
-      // width: '20vh',
-    },
-  }));
-  const theme = createTheme({
-    typography: {
-      htmlFontSize: 18,
+  // const NavBox = styled(Box)<BoxProps>(({ theme }) => ({
+  //   color: theme.palette.getContrastText(blueGrey[900]),
+  //   height: '100vh',
+  //   width: '6%',
+  //   borderRight: '1px solid #d1c4e9',
+  //   transition: 'all .6s',
+  //   position: 'absolute',
+  //   '&:hover': {
+  //     backgroundColor: '#022c33',
+  //     width: '12%',
+  //     borderRight: 'none',
+  //   },
+  // }));
+  // const AvtarBox = styled(Box)<BoxProps>(({ theme }) => ({
+  //   color: theme.palette.getContrastText(blueGrey[900]),
+  //     display: 'flex',
+  //     alignItems: 'center',
+  //     cursor: 'pointer',
+  //   '&:hover': {
+  //     // backgroundColor: blueGrey[700],
+  //     // width: '20vh',
+  //   },
+  // }));
+  // const theme = createTheme({
+  //   typography: {
+  //     htmlFontSize: 18,
       
-    },
-  });
+  //   },
+  // });
   return (
     <LayoutBox>
-    <NavBox onMouseEnter={() => setNavOpen(true)} onMouseLeave={() => setNavOpen(false)} >
-    <Box sx={{ width: '100%', paddingTop: '15em', paddingLeft: '1em' }}>
+    <Box sx={NavBox} onMouseEnter={() => setNavOpen(true)} onMouseLeave={() => setNavOpen(false)} >
+    <Box >
     <Stack spacing={2}>
-    <ThemeProvider theme={theme}>
+    <Box sx={NaveBoxInner}>
       {activePage !== 'About' ?
-      <Box sx={navImg}>
-        <Avatar alt="Hazem Kawas" src={faceImg} sx={{ width: 50, height: 50 }} />
+      <Box sx={AvtarBox}>
+        <Avatar alt="Hazem Kawas" src={faceImg} sx={navImg} />
       </Box> : null }
       <Box onClick={() => setActivePage('About')}>
-    <AvtarBox  >
+    <Box  sx={AvtarBox}>
       <Avatar sx={{ bgcolor: 'rgba(33, 253, 253, 0.255)' }}  ><HomeOutlinedIcon/></Avatar >
-      {navOpen ? <Typography sx= {{paddingLeft: '.3em'}} >Home</Typography> : null }
-    </AvtarBox>
+      {navOpen ? <Typography sx= {text} >About</Typography> : null }
+    </Box>
       </Box>
-      <Box onClick={() => setActivePage('Experiences')}>
-        <AvtarBox  >
+      <Box  onClick={() => setActivePage('Experiences')}>
+        <Box sx={AvtarBox} >
         <Avatar sx={{ bgcolor: 'rgba(33, 253, 253, 0.255)' }} ><ListAltIcon/></Avatar  >
-        {navOpen ? <Typography sx= {{paddingLeft: '.3em'}} >Experiences</Typography> : null }
-        </AvtarBox>
+        {navOpen ? <Typography sx= {text} >Experiences</Typography> : null }
+        </Box>
       </Box>
       <Box onClick={() => setActivePage('skills')}>
-        <AvtarBox  >
+        <Box sx={AvtarBox} >
         <Avatar sx={{ bgcolor: 'rgba(33, 253, 253, 0.255)' }} ><ListAltIcon/></Avatar  >
-        {navOpen ? <Typography sx= {{paddingLeft: '.3em'}} >Skills</Typography> : null }
-        </AvtarBox>
+        {navOpen ? <Typography sx= {text} >Skills</Typography> : null }
+        </Box>
       </Box>
       <Box onClick={() => setActivePage('Portfolio')}>
-      <AvtarBox   >
+      <Box   sx={AvtarBox}>
         <Avatar sx={{ bgcolor: 'rgba(33, 253, 253, 0.255)' }} ><AccountBoxIcon/></Avatar  >
-        {navOpen ? <Typography sx= {{paddingLeft: '.3em'}} >Portfolio</Typography> : null }
-        </AvtarBox>
+        {navOpen ? <Typography sx= {text} >Portfolio</Typography> : null }
+        </Box>
       </Box>
       <Box onClick={() => setActivePage('Contact')}>
-      <AvtarBox  >
+      <Box sx={AvtarBox} >
         <Avatar sx={{ bgcolor: 'rgba(33, 253, 253, 0.255)' }} ><ContactMailIcon/></Avatar >
-        {navOpen ? <Typography sx= {{paddingLeft: '.3em'}} >Contact</Typography> : null }
-        </AvtarBox>
+        {navOpen ? <Typography sx= {text} >Contact</Typography> : null }
+        </Box>
       </Box>
-    </ThemeProvider>
+    </Box>
     </Stack>
   </Box>
-  </NavBox>
+  </Box>
   <Box sx={bodyBoxStyle}>
         {/* {activePage === 'Home' && <Home />} */}
         {activePage === 'About' && <AboutMe />}
@@ -118,6 +118,45 @@ const NavBar = () => {
     
   )
 }
+const NavBox: SxProps = {
+  height: {xs: '12vh', md: '10vh', lg: '100vh'},
+  width: {xs: '100%', md: '100%', lg: '10%'},
+  borderRight: {xs: 'none', md: 'none', lg: '1px solid #d1c4e9'},
+  transition: 'all .6s',
+  position: {xs: 'absolute', md: 'relative', lg: 'relative'},
+  top: 0,
+  left: 0,
+  '&:hover': {
+    // backgroundColor: '#022c33',
+    // width: '15%',
+    borderRight: 'none',
+  },
+}
+const NaveBoxInner: SxProps = {
+  height: {xs: '12vh', md: '10vh', lg: '40vh'},
+  width: {xs: '100%', md: '100%', lg: '6%'}, 
+  paddingTop: {xs: '0em', md: '0em', lg: '15em'}, 
+  paddingLeft: {xs: '0em', md: '0em', lg: '1em'}, 
+  display: 'flex',
+  flexDirection: {xs: 'row', md: 'row', lg: 'column'},
+  justifyContent: {xs: 'space-around', md: 'center', lg: 'space-between'},
+  alignItems: {xs: 'center', md: 'center', lg: 'flex-start'},
+  // position: {xs: 'absolute', md: 'relative', lg: 'relative'},
+  // top: 0,
+  // left: 0,
+}
+const text: SxProps = {
+  paddingLeft: '.5em', 
+  fontSize: '1em'
+}
+const AvtarBox: SxProps = {
+  display: 'flex',
+  Width: {xs: '100%', md: '100%', lg: '100%'},
+  flexDirection:  {xs: 'column', md: 'column', lg: 'row'},
+  justifyContent: {xs: 'space-between', md: 'center', lg: 'space-between'},
+  alignItems: 'center',
+  cursor: 'pointer',
+}
 const bodyBoxStyle: SxProps = {
   width: { xs: '100%', md: '80%',lg: '100%' },
   display: 'flex',
@@ -125,7 +164,13 @@ const bodyBoxStyle: SxProps = {
 }
 const navImg: SxProps = {
   position: "absolute",
-  top: "30%",
-
+  top: "25%",
+  left: "1em",
+  display: {xs: 'none', md: 'none', lg: 'block'},
+  width: 50, 
+  height: 50
 }
+// const box: SxProps = {
+//  width: '100%',
+// }
 export default NavBar
