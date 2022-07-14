7478b8c9aecb2eee929c012d4777b681add0f1b8
Hazem Kawas <hazem.k.4488@gmail.com>
2022-07-14 23:53:34 +0200

    general fixes

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./AboutMe/AboutMe";
-import './App.css';
 import Layout from "./Layout/Layout";
 import StartPage from "./StartPage/StartPage";
 
@@ -8,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path="/Aboutme" element={<StartPage />}></Route>
     <Route path="/HazemCV" element={<Layout />}></Route>
+    <Route path="/Aboutme" element={<StartPage />}></Route>
     </Routes>
     </BrowserRouter>
 
