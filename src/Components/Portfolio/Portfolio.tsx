3beb3378ed296801204dfccf5e923bc5343e4dc1
Hazem Kawas <hazem.k.4488@gmail.com>
2022-07-21 00:01:35 +0200

    update skills

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -5,12 +5,28 @@ import React from 'react'
 // img
 import cloud1 from '../../img/img/clouds/Small Single Cloud.png'
 import cloud2 from '../../img/img/clouds/cloud6.png'
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 const Portfolio = () => {
+  const swiper = new Swiper('.swiper', {
+    // Install modules
+    modules: [Navigation, Pagination, Scrollbar],
+    speed: 500,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // ...
+  });
   return (
     <>
-   <Box>
-    |Portfolio
-   </Box>
+      <Box>
+        Portfolio
+      </Box>
     </>
   )
 }
--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,41 +1,128 @@
 import { Box, BoxProps, styled, SxProps } from '@mui/material'
 import { blueGrey } from '@mui/material/colors';
 import backGroundImage from '../../img/Portfolio/Animal-WarZone/Picture1.png'
-import React from 'react'
-import WordCloud from "react-wordcloud"
-import Cloud from "react-wordcloud"
-import Sidebar from "react-wordcloud"
+import TS from '../../img/img/skills/Group 2.png'
+import React, { useEffect } from 'react'
+import './Skills.css'
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+const size = 2;
+const test = 400
+const speed = {
+  x: 0.4,
+  y: 0.2,
+}
+
+const position = {
+  top: 0,
+  left: size,
+}
+
+const rest = [
+  {
+    img: `${TS}`
+  },
+  {
+    img: `${TS}`
+  },
+  {
+    img: `${TS}`
+  },
+  {
+    img: `${TS}`
+  },
+]
+
+
 
 const Skills = () => {
+
   const LayoutBox = styled(Box)<BoxProps>(({ theme }) => ({
-    color: theme.palette.getContrastText(blueGrey[900]),
     height: '100vh',
-    display: 'flex',
+    width: '80%',
+    // display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     '&:hover': {
       // backgroundColor: blueGrey[700],
     },
   }));
-  return (
-    <>
-    <LayoutBox >
-      <Box sx={MainBox}>
 
-      </Box>
+ 
+  const bouncing = () => {
+    const ballBox  = document.createElement('ul')
+    ballBox.className = 'ballbox'
+    for (let i = 0; i < rest.length; i++) {
+      const skill = rest[i]
+      const ball = document.createElement('li')
+      ball.className = 'ball'
+      const skillImg = document.createElement('img')
+      
+      skillImg.src = skill.img
+      ballBox?.appendChild(ball)
+      ball?.appendChild(skillImg)
+      skillImg.style.left = Math.random() * WIDTH + 'px';
+      // skillImg.style.width = size + '%';;
+      const skillBox =   document.querySelector('.bubbleBox')?.appendChild(ballBox)
+      
+      skillImg.style.position =  'fixed'
+      console.log(ballBox);           
+      console.log('skillBox', skillBox);
+
+      
+      
+    }
+    const update = () => {
+      speed.x = .05 * Math.random();
+      speed.y = .05 * Math.random();
+    }
+  const move = () => {
+      position.left =  +speed.x;
+
+      if (position.left > WIDTH) {
+        position.left = -size;
+        console.log( 'position.left', position.left);
+      } 
+      else if (position.left < -test) {
+        position.left = -100;   
+        console.log( 'position.left', position.left);
+     }
+    //  skillImg.style.left = position.left + '%';
+
+  //    position.top +=  speed.y;
+  //    if (position.top > 500) {
+  //     position.top = -size;
+  // }else if (position.top < -200) {
+  //     position.top = 100;   
+  //  }
+  //  skillImg.style.top = position.top + '%';
+  }
+  setInterval(update, 1000);
+  setInterval(move, 20);
+  
+}
+// bouncing()
+const MINUTE_MS = 600;
+
+useEffect(() => {
+  bouncing()
+  
+}, )
+
+    return (
+      <>
+    <LayoutBox >
+    <Box sx={main} className='bubbleBox' >
+    
+
+    </Box>
     </LayoutBox>
     </>
   )
 }
-const MainBox: SxProps = {
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  alignSelf: 'center',
-  justifyContent: 'center',
-  backgroundSize: 'cover',
-  color: '#022c33',
-
+const main: SxProps = {
+  zIndex: '3'
 }
 const BoxStyle: SxProps = {
   width: { xs: '100%', md: '80%',lg: '100%' },
@@ -44,6 +131,6 @@ const BoxStyle: SxProps = {
   alignItems: 'center',
   justifyContent: "center",
   backgroundImage: backGroundImage,
-
+  
 }
 export default Skills
